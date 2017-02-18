6277696cdb4a110901684364463f9b36a2951220
Laura Catano <lllaura@cs.utexas.edu>
2017-02-18 13:41:03 -0600

    kdtree method stubs

--- src/scene/kdTree.h
+++ src/scene/kdTree.h
@@ -6,74 +6,14 @@
 // 	SplitNode;
 // 	LeafNode;
 // };
-
-class SplitNode
-{
-public: 
-	//Axis and Position needs types
-	//axis
-	//position
-	//kdTree* left
-	//kdTree* right
-
-	/*bool findIntersection(r, i, t_min, t_max){
-
-		//The check for ray is parallel sounds like r.getDirection() cross/dot axis == 1/0 i forget which gives parallel, but yeah.
-		if(ray is nearly parallel){
-
-			//Skip this intersection??
-			calculate as near parallel()
-		}else{
-			// Idk what it means by beyond and before...
-			if(splitPlane.position is beyond ray.position){
-				if(left->findIntersection(r, i, t_min, t_max))
-					return true;
-			}
-			else if(splitPlane.position is before ray.position){
-				if(right->findIntersection(r, i, t_min, t_max))
-					return true;
-			}
-			else{
-				if (left→findIntersection(r, i, t_min, t_max)) return true;
-				if (right→findIntersection(r, i, t_min, t_max)) return true;
-			}
-			return false;
-		}
-	}*/
-
-	//Initiate the Stuff in here, though I forget if C++ yells at you for putting this after the function. might need to have this before findIntersection
-	SplitNode();
-	~SplitNode();
-
-};
-class LeafNode
-{
-public:
-
-	// beginObjects() from the scene.h to populate this objList 
-	//objList;
-
-	/*void findIntersection(r, i, t_min, t_max){
-		//For each loop, needs obj type.
-		for(<t> obj: objList){
-			isect c_i
-			if(obj.intersect(r, c_i) && c_i.t >= t_min && c_i.t <= t_max ){
-				i = c_i;
-			}
-		}
-	}*/
-	// Initate the objList in here, same as the splitnode if it yells.
-	LeafNode();
-	~LeafNode();
-};
 class kdTree
 {
 public:
 
 
 	//This one is a bit weird. It is returning a node type, might need to uncomment and implement the struct.
-	/*function buildTree(objList, boundingBox, depthLimit, leafSize){
-		if (objList.size <= leafSize or ++depth == depthLimit){ 
+	void buildTree(std::vector<Geometry*> objList, BoundingBox* bbox, int depthLimit, int leafSize){
+		/*if (objList.size <= leafSize or ++depth == depthLimit){ 
 			return LeafNode(objList)
 		}
 		bestPlane ← findBestSplitPlane(objList, boundingBox);
@@ -93,11 +33,11 @@ public:
 			return	LeafNode(objList);
 		else return SplitNode(bestPlane.position, bestPlane.axis,
 		buildTree(leftList, bestPlane.leftBBox, depth, leafSize),
-		buildTree(rightList, bestPlane.rightBBox, depth, leafSize));
-	}*/
+		buildTree(rightList, bestPlane.rightBBox, depth, leafSize));*/
+	}
 
-	/*function findBestSplitPlane(objList, boundingBox){
-		for each axis:
+	int findBestSplitPlane(std::vector<Geometry*> objList, BoundingBox* bbox){
+		/*for each axis
 			for each object:
 			SplitPlane p1.position = obj.bBMinOnAxis
 			SplitPlane p2.position = obj.bBMaxOnAxis
@@ -114,7 +54,69 @@ public:
 			if (SAM < minSam) then
 				minSam = SAM
 				bestPlane = plane
-		return bestPlane;
-	}*/
+		return bestPlane;*/
+		return 0;		
+	}
 
+};
+
+class SplitNode : public kdTree
+{
+public: 
+	char axis;
+	int position;
+	kdTree* left;
+	kdTree* right;
+
+	bool findIntersection(ray& r, isect& i, int t_min, int t_max){
+
+		/*//The check for ray is parallel sounds like r.getDirection() cross/dot axis == 1/0 i forget which gives parallel, but yeah.
+		if(ray is nearly parallel){
+
+			//Skip this intersection??
+			calculate as near parallel()
+		}else{
+			// Idk what it means by beyond and before...
+			if(splitPlane.position is beyond ray.position){
+				if(left->findIntersection(r, i, t_min, t_max))
+					return true;
+			}
+			else if(splitPlane.position is before ray.position){
+				if(right->findIntersection(r, i, t_min, t_max))
+					return true;
+			}
+			else{
+				if (left→findIntersection(r, i, t_min, t_max)) return true;
+				if (right→findIntersection(r, i, t_min, t_max)) return true;
+			}
+			return false;
+		}*/
+		return false;	
+	}
+
+	//Initiate the Stuff in here, though I forget if C++ yells at you for putting this after the function. might need to have this before findIntersection
+	SplitNode();
+	~SplitNode();
+
+};
+class LeafNode : public kdTree
+{
+public:
+
+	// beginObjects() from the scene.h to populate this objList 
+	std::vector<Geometry*> objList;
+
+	bool findIntersection(ray& r, isect& i, int t_min, int t_max){
+		/*//For each loop, needs obj type.
+		for(<t> obj: objList){
+			isect c_i
+			if(obj.intersect(r, c_i) && c_i.t >= t_min && c_i.t <= t_max ){
+				i = c_i;
+			}
+		}*/
+		return false;	
+	}
+	// Initate the objList in here, same as the splitnode if it yells.
+	LeafNode();
+	~LeafNode();
 };
